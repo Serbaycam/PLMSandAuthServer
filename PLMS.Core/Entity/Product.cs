f3005d1a3a868e2a1ba05b59606855b0cc7df1a8
Serbay ÇAM <serbaycam@gmail.com>
2023-10-17 21:15:52 +0300

    Launchsettings düzenlendi, libman düzenlendi,appsettings düzenlendi.

--- PLMS.Core/Entity/Product.cs
+++ PLMS.Core/Entity/Product.cs
@@ -2,10 +2,10 @@
 {
     public class Product:BaseEntity
     {
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public int Stock { get; set; }
         public decimal Price { get; set; }
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public required Category Category { get; set; }
     }
 }
