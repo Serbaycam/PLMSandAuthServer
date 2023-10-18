16733350fa9b2f6ce720af4f93b82679c916fa38
Serbay ÇAM <serbaycam@gmail.com>
2023-10-18 20:44:25 +0300

    Arealar eklendi

---
+++ PLMS.Web/Areas/Account/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PLMS.Web.Areas.Account.Controllers
+{
+    [Area("Account")]
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- PLMS.Web/Program.cs
+++ PLMS.Web/Program.cs
@@ -43,9 +43,18 @@ namespace PLMS.Web
 
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area}/{controller}/{action}/{id?}"
+                );
+
+                endpoints.MapControllerRoute(
+                    name:"default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                    );
+            });
 
             app.Run();
         }
