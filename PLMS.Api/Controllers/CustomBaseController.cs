6c6ef3f37c810624e12f7c0aa223690e1d800e06
Serbay <serbaycam@erateks.com>
2023-10-20 13:40:38 +0300

    api/[controller]/[action] eklendi.

--- PLMS.Api/Controllers/CustomBaseController.cs
+++ PLMS.Api/Controllers/CustomBaseController.cs
@@ -4,7 +4,7 @@ using PLMS.Core.DTOs.CustomResponse;
 
 namespace PLMS.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class CustomBaseController : ControllerBase
     {
