16e0f74c9db24e0efe503c5ae9650da5f0f9de5e
Serbay ÇAM <serbaycam@gmail.com>
2023-10-27 00:22:03 +0300

    register dto güncellendi

--- AuthIdentity.Core/DTOs/AuthIdentityUserRegisterDto.cs
+++ AuthIdentity.Core/DTOs/AuthIdentityUserRegisterDto.cs
@@ -6,23 +6,24 @@ namespace AuthIdentity.Core.DTOs
     {
         public AuthIdentityUserRegisterDto()
         {
-            
+
         }
         public AuthIdentityUserRegisterDto(string name, string surName, string email, string password, string passwordMatch)
         {
             Name = name;
-            SurName = surName;
+            Surname = surName;
             Email = email;
             Password = password;
             PasswordMatch = passwordMatch;
         }
 
         public string Name { get; set; }
-        public string SurName { get; set; }
+        public string Surname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
 
         [Compare(nameof(Password))]
         public string PasswordMatch { get; set; }
+        public string UserName => Name+" "+Surname;
     }
 }
