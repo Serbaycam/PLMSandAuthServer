641796ad64c866fa0f13139e28d3df5c77aed883
Serbay <serbaycam@erateks.com>
2023-10-27 08:12:34 +0300

    member controllerdaki create metodundan mapper kaldırıldı

--- AuthIdentity.Service/Services/AuthIdentityMemberService.cs
+++ AuthIdentity.Service/Services/AuthIdentityMemberService.cs
@@ -6,42 +6,49 @@ using Microsoft.AspNetCore.Identity;
 
 namespace AuthIdentity.Service.Services
 {
-	public class AuthIdentityMemberService : IAuthIdentityMemberService
-	{
-		private readonly UserManager<AuthIdentityUser> _userManager;
-		private readonly SignInManager<AuthIdentityUser> _signInManager;
-		private readonly IMapper _mapper;
+    public class AuthIdentityMemberService : IAuthIdentityMemberService
+    {
+        private readonly UserManager<AuthIdentityUser> _userManager;
+        private readonly SignInManager<AuthIdentityUser> _signInManager;
+        private readonly IMapper _mapper;
 
-		public AuthIdentityMemberService(UserManager<AuthIdentityUser> userManager)
-		{
-			_userManager = userManager;
-		}
+        public AuthIdentityMemberService(UserManager<AuthIdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
 
-		public async Task<AuthIdentityUserDto> GetUserDtoByUserNameAsync(string userName)
-		{
-			AuthIdentityUser currentUser = (await _userManager.FindByNameAsync(userName));
-			AuthIdentityUserDto userDto = _mapper.Map<AuthIdentityUserDto>(currentUser);
-			return userDto;
-		}
+        public async Task<AuthIdentityUserDto> GetUserDtoByUserNameAsync(string userName)
+        {
+            AuthIdentityUser currentUser = (await _userManager.FindByNameAsync(userName));
+            AuthIdentityUserDto userDto = _mapper.Map<AuthIdentityUserDto>(currentUser);
+            return userDto;
+        }
 
-		public async Task<(bool,IEnumerable<IdentityError>)> RegisterUserByUserDtoAsync(AuthIdentityUserRegisterDto authIdentityUserRegisterDto)
-		{
-			AuthIdentityUser user = _mapper.Map<AuthIdentityUser>(authIdentityUserRegisterDto);
-			IdentityResult identityResult = await _userManager.CreateAsync(user, authIdentityUserRegisterDto.Password);
-			if(!identityResult.Succeeded)
-			{
-				return (false, identityResult.Errors);
-			}
-			else
-			{
-				return (true,null);
-			}
+        public async Task<(bool, IEnumerable<IdentityError>)> RegisterUserByUserDtoAsync(AuthIdentityUserRegisterDto authIdentityUserRegisterDto)
+        {
+            IdentityResult identityResult = await _userManager.CreateAsync(
+                new AuthIdentityUser
+                {
+                    Email = authIdentityUserRegisterDto.Email,
+                    Name = authIdentityUserRegisterDto.Name,
+                    Surname = authIdentityUserRegisterDto.Surname,
+                    UserName = authIdentityUserRegisterDto.UserName,
 
-		}
+                }, authIdentityUserRegisterDto.Password);
+            if (!identityResult.Succeeded)
+            {
+                return (false, identityResult.Errors);
+            }
+            else
+            {
+                return (true, null);
+            }
 
-		public async Task Logout()
-		{
-			await _signInManager.SignOutAsync();
-		}
-	}
+        }
+
+        public async Task Logout()
+        {
+            await _signInManager.SignOutAsync();
+        }
+    }
 }
