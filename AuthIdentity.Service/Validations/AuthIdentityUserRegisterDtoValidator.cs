30a7791bb2cf3bc1778bab4bdb6eb45856c4d17a
Serbay ÇAM <serbaycam@gmail.com>
2023-10-27 20:21:42 +0300

    AuthIdentityUserRegisterDtoValidator tamamlandı login register tekrar düzenlendi validation hataları gösterildi.

--- AuthIdentity.Service/Validations/AuthIdentityUserRegisterDtoValidator.cs
+++ AuthIdentity.Service/Validations/AuthIdentityUserRegisterDtoValidator.cs
@@ -10,8 +10,8 @@ namespace AuthIdentity.Service.Validations
             RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is required.").MinimumLength(2).WithMessage("{PropertyName} can be 2 character min.").MaximumLength(50).WithMessage("{PropertyName} can be 50 characters max.").NotEmpty().WithMessage("{PropertyName} cannot be empty");
             RuleFor(x => x.Surname).NotNull().WithMessage("{PropertyName} is required.").MinimumLength(2).WithMessage("{PropertyName} can be 2 character min.").MaximumLength(50).WithMessage("{PropertyName} can be 50 characters max.").NotEmpty().WithMessage("{PropertyName} cannot be empty");
             RuleFor(x => x.Email).NotNull().WithMessage("{PropertyName} is required.").NotEmpty().WithMessage("{PropertyName} cannot be empty");
-            RuleFor(x => x.Password).NotNull().WithMessage("{PropertyName} is required.").Matches(x=>x.PasswordMatch).WithMessage("Passwords must be same").NotEmpty().WithMessage("{PropertyName} cannot be empty");
-            RuleFor(x => x.PasswordMatch).NotNull().WithMessage("{PropertyName} is required.").Matches(x => x.Password).WithMessage("Passwords must be same").NotEmpty().WithMessage("{PropertyName} cannot be empty");
+            RuleFor(x => x.Password).NotNull().WithMessage("{PropertyName} is required.").NotEmpty().WithMessage("{PropertyName} cannot be empty");
+            RuleFor(x => x.PasswordMatch).NotNull().WithMessage("{PropertyName} is required.").Equal(x => x.Password).WithMessage("Passwords must be same").NotEmpty().WithMessage("{PropertyName} cannot be empty");
         }
     }
 }
