4c94e1ad1ec9127437fe900d6873bfdaa14975a5
Serbay ÇAM <serbaycam@gmail.com>
2023-11-15 19:09:59 +0300

    Appnoauth ve app apileri eklendi

---
+++ AuthIdentityJWTApp.Api/Controllers/StockController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AuthIdentityJWTApp.Api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStock()
+        {
+            var userName = HttpContext.User.Identity.Name;
+            var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+            return Ok($"UserName : {userName}, UserId : {userIdClaim.Value}");
+        }
+    }
+}
