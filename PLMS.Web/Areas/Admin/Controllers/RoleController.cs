a0dbf19a2c9882674fa7f5d454749508058d5c14
Serbay ÇAM <serbaycam@gmail.com>
2023-12-23 18:13:14 +0300

    Primary Constructorlar tanımlandı

--- PLMS.Web/Areas/Admin/Controllers/RoleController.cs
+++ PLMS.Web/Areas/Admin/Controllers/RoleController.cs
@@ -2,13 +2,10 @@
 {
     [Area("Admin")]
     [Authorize]
-    public class RoleController : Controller
+    public class RoleController(RoleManager<AuthIdentityRole> roleManager) : Controller
     {
-        private readonly RoleManager<AuthIdentityRole> _roleManager;
-        public RoleController(RoleManager<AuthIdentityRole> roleManager)
-        {
-            _roleManager = roleManager;
-        }
+        private readonly RoleManager<AuthIdentityRole> _roleManager = roleManager;
+
         public async Task<IActionResult> Roles()
         {
             List<AuthIdentityRole> roles = await _roleManager.Roles.ToListAsync();
--- PLMS.Web/Controllers/HomeController.cs
+++ PLMS.Web/Controllers/HomeController.cs
@@ -1,17 +1,11 @@
 ﻿namespace PLMS.Web.Controllers
 {
     [Authorize]
-    public class HomeController : Controller
+    public class HomeController(ILogger<HomeController> logger, IGenericService<SizeSet, PLMSDbContext> genericService, IMapper mapper) : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly IMapper mapper;
-        private readonly IGenericService<SizeSet, PLMSDbContext> genericService;
-        public HomeController(ILogger<HomeController> logger, IGenericService<SizeSet, PLMSDbContext> genericService, IMapper mapper)
-        {
-            _logger = logger;
-            this.genericService = genericService;
-            this.mapper = mapper;
-        }
+        private readonly ILogger<HomeController> _logger = logger;
+        private readonly IMapper mapper = mapper;
+        private readonly IGenericService<SizeSet, PLMSDbContext> genericService = genericService;
 
         public IActionResult Index()
         {
